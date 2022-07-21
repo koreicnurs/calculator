52501aeef3c86c7a781df9c31be6b5bc703f277b
koreicnurs <koreicnurs@gmail.com>
2022-07-21 10:17:23 +0600

    create html code for buttons and add logic which we can enter code and cheking to correct success

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,15 +3,35 @@ import {useDispatch, useSelector} from "react-redux";
 import './Counter.css';
 
 const Counter = () => {
-    const counter = useSelector(state => state.counter);
+    const history = useSelector(state => state.numbers);
+    const status = useSelector(state => state.status);
     const dispatch = useDispatch();
 
-    const inc = () => dispatch({type: 'INC'});
+    const addNumber = (number) => dispatch({type: 'ADD', payload: number});
+    const result = () => dispatch({type: 'RES'});
+    const del = () => dispatch({type: 'DEL'});
+
 
     return (
-        <div className='Counter'>
-            <h1>{counter}</h1>
-            <button onClick={inc}>inc</button>
+        <div className="enter">
+            <div className="top">
+                <div className={status ? 'screen green' : 'screen'}>{history.replace(/./gm, '*')}</div>
+            </div>
+
+            <div className="keys">
+                <button onClick={() => addNumber(1)}>1</button>
+                <button onClick={() => addNumber(2)}>2</button>
+                <button onClick={() => addNumber(3)}>3</button>
+                <button onClick={() => addNumber(4)}>4</button>
+                <button onClick={() => addNumber(5)}>5</button>
+                <button onClick={() => addNumber(6)}>6</button>
+                <button onClick={() => addNumber(7)}>7</button>
+                <button onClick={() => addNumber(8)}>8</button>
+                <button onClick={() => addNumber(9)}>9</button>
+                <button className='clear' onClick={del}>&lt;</button>
+                <button onClick={() => addNumber(0)}>0</button>
+                <button className='eval' onClick={result}>=</button>
+            </div>
         </div>
     );
 };
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,34 @@
 const initialStore = {
-    counter: 0,
+    numbers: '',
+    pass: '2222',
+    status: false,
 };
 
 const reducer =  (state = initialStore, action) => {
-    if (action.type === 'INC') {
-        return {...state, counter: state.counter + 10}
+    if (action.type === 'ADD') {
+        if(state.numbers.length < 4){
+
+            return {...state, numbers: state.numbers + action.payload};
+        } else {
+            return state;
+        }
+    }
+
+    // if (action.type === 'OP') {
+    //     return {...state, numbers: state.numbers + action.payload};
+    // }
+
+    if (action.type === 'RES') {
+        if(state.numbers === state.pass) {
+            return {...state, status: true};
+        } else {
+            return state;
+        }
+
+    }
+
+    if (action.type === 'DEL') {
+        return {...state, numbers: state.numbers.substring( 0, state.numbers.length -1 ), status: false};
     }
 
     return state;
